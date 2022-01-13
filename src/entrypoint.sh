639f332fb777d78808c11c488b42b4d1fa97aec6
Gianni Carafa <gianni.carafa@srf.ch>
2022-01-13 17:26:52 +0100

    add some kubeaudit flags

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,24 +3,50 @@
 set -e
 set -o pipefail
 
-HELM_VER=${3}
+KUBEAUDIT_COMMANDS=${1:-all}
+PATH=${2}
+HELM_VER${3}
+KUBEAUDIT_VER=${4}
+KUBEAUDIT_INCLUDEGENEREATED=${5}
+KUBEAUDIT_FORMAT=${6}
+KUBEAUDIT_MINSEVERITY=${7}
+
 if [[ "${HELM_VER}" != "" ]]; then
   curl -sL https://get.helm.sh/helm-v${HELM_VER}-linux-amd64.tar.gz | \
   tar xz && mv linux-amd64/helm /usr/local/bin/helm && rm -rf linux-amd64
 fi
 
-HELM3_VER=${4}
-if [[ "${HELM3_VER}" != "" ]]; then
-  curl -sL https://get.helm.sh/helm-v${HELM3_VER}-linux-amd64.tar.gz | \
-  tar xz && mv linux-amd64/helm /usr/local/bin/helmv3 && rm -rf linux-amd64
-fi
-
-KUBEAUDIT_VER=${5}
-if [[ "${HELM3_VER}" != "" ]]; then
+if [[ "${KUBEAUDIT_VER}" != "" ]]; then
   curl -sSL https://github.com/Shopify/kubeaudit/releases/download/${KUBEAUDIT_VER}/kubeaudit_${KUBEAUDIT_VER}_linux_amd64.tar.gz | \
   tar xz && mv kubeaudit /usr/local/bin/kubeaudit
 fi
 
 echo ">>> Executing command <<<"
+if [[ "${PATH}" == "" ]]; then
+  echo "No path provided"
+  exit 1
+fi
 
-bash -c "set -e;  set -o pipefail; helm template ${1} > manifest.yaml | kubeaudit ${2} -f manifest.yaml"
+if [[ "${COMMANDS}" == "" ]]; then
+  echo "No commands provided"
+  exit 1
+fi
+
+if [[ "${KUBEAUDIT_INCLUDEGENEREATED}" != "" ]]; then
+  KUBEAUDIT_INCLUDEGENEREATED="--includegenerated"
+fi
+
+if [[ "${KUBEAUDIT_FORMAT}" != "" ]]; then
+  KUBEAUDIT_FORMAT="--format ${KUBEAUDIT_FORMAT}"
+fi
+
+if [[ "${KUBEAUDIT_MINSEVERITY}" != "" ]]; then
+  KUBEAUDIT_MINSEVERITY="--minseverity ${KUBEAUDIT_MINSEVERITY}"
+fi
+
+bash -c "
+set -e;
+set -o pipefail; 
+helm template ${PATH} > manifest.yaml
+kubeaudit ${KUBEAUDIT_COMMANDS} ${KUBEAUDIT_INCLUDEGENEREATED} ${KUBEAUDIT_FORMAT} ${KUBEAUDIT_MINSEVERITY} -f manifest.yaml
+"
